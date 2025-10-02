8775415c353055915f8b1e615a3fa8f197ee66a2
amoyament <amoyamen44@gmail.com>
2025-10-02 13:19:27 -0400

    testing podman

--- setup-automation/setup-podman.sh
+++ setup-automation/setup-podman.sh
@@ -5,20 +5,20 @@
 
 nmcli connection add type ethernet con-name eth1 ifname eth1 ipv4.addresses 192.168.1.11/24 ipv4.method manual connection.autoconnect yes
 nmcli connection up eth1
-echo "192.168.1.10 control.lab control aap control.ansible.workshop" >> /etc/hosts
-echo "192.168.1.11 podman.lab podman" >> /etc/hosts
+# echo "192.168.1.10 control.lab control aap control.ansible.workshop" >> /etc/hosts
+# echo "192.168.1.11 podman.lab podman" >> /etc/hosts
 
-retry() {
-    for i in {1..3}; do
-        echo "Attempt $i: $2"
-        if $1; then
-            return 0
-        fi
-        [ $i -lt 3 ] && sleep 5
-    done
-    echo "Failed after 3 attempts: $2"
-    exit 1
-}
+# retry() {
+#     for i in {1..3}; do
+#         echo "Attempt $i: $2"
+#         if $1; then
+#             return 0
+#         fi
+#         [ $i -lt 3 ] && sleep 5
+#     done
+#     echo "Failed after 3 attempts: $2"
+#     exit 1
+# }
 
 retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
 retry "update-ca-trust"
