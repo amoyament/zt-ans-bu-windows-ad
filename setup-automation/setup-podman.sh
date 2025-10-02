7f4f43cab259533e0790b327cc56a43b62017072
amoyament <amoyamen44@gmail.com>
2025-10-02 14:19:10 -0400

    test podman

--- setup-automation/setup-podman.sh
+++ setup-automation/setup-podman.sh
@@ -8,17 +8,17 @@ nmcli connection up eth1
 # echo "192.168.1.10 control.lab control aap control.ansible.workshop" >> /etc/hosts
 # echo "192.168.1.11 podman.lab podman" >> /etc/hosts
 
-# retry() {
-#     for i in {1..3}; do
-#         echo "Attempt $i: $2"
-#         if $1; then
-#             return 0
-#         fi
-#         [ $i -lt 3 ] && sleep 5
-#     done
-#     echo "Failed after 3 attempts: $2"
-#     exit 1
-# }
+retry() {
+    for i in {1..3}; do
+        echo "Attempt $i: $2"
+        if $1; then
+            return 0
+        fi
+        [ $i -lt 3 ] && sleep 5
+    done
+    echo "Failed after 3 attempts: $2"
+    exit 1
+}
 
 retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
 retry "update-ca-trust"
@@ -35,7 +35,7 @@ tee /tmp/setup.yml << EOF
 ### Podman setup 
 ###
 - name: Setup podman and services
-  hosts: localhost
+  hosts: podman
   gather_facts: no
   #become: true
   tasks:
