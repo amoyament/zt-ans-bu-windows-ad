7a747a3b51945634613374500eadc4594a790f1b
amoyament <amoyamen44@gmail.com>
2025-10-09 15:28:50 -0400

    test gitea

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -116,7 +116,7 @@ EOF
 
 # # Execute the setup playbooks
 echo "=== Running Git/Gitea Setup ==="
-ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -e @/tmp/track-vars.yml -i /tmp/inventory /tmp/git-setup.yml
+# ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -e @/tmp/track-vars.yml -i /tmp/inventory /tmp/git-setup.yml
 
 
 # SET UP WINDOWS (migrated from setup-windows.sh)
