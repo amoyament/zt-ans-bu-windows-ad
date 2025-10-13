5d0888e2cbffb4d67fba864f1122631fa0312b1d
amoyament <amoyamen44@gmail.com>
2025-10-13 11:32:55 -0400

    removed user and org creation

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -319,153 +319,157 @@ cat <<EOF | tee /tmp/controller-setup.yml
   gather_facts: true
     
   tasks:
-   # Create auth login token
-    - name: get auth token and restart automation-controller if it fails
-      block:
-        - name: Refresh facts
-          setup:
+#    # Create auth login token
+#     - name: get auth token and restart automation-controller if it fails
+#       block:
+#         - name: Refresh facts
+#           setup:
 
-        - name: Create oauth token
-          ansible.controller.token:
-            description: 'Instruqt lab'
-            scope: "write"
-            state: present
-            controller_host: controller
-            controller_username: "{{ controller_admin_user }}"
-            controller_password: "{{ controller_admin_password }}"
-            validate_certs: false
-          register: _auth_token
-          until: _auth_token is not failed
-          delay: 3
-          retries: 5
-      rescue:
-        - name: In rescue block for auth token
-          debug:
-            msg: "failed to get auth token. Restarting automation controller service"
+#         - name: Create oauth token
+#           ansible.controller.token:
+#             description: 'Instruqt lab'
+#             scope: "write"
+#             state: present
+#             controller_host: controller
+#             controller_username: "{{ controller_admin_user }}"
+#             controller_password: "{{ controller_admin_password }}"
+#             validate_certs: false
+#           register: _auth_token
+#           until: _auth_token is not failed
+#           delay: 3
+#           retries: 5
+#       rescue:
+#         - name: In rescue block for auth token
+#           debug:
+#             msg: "failed to get auth token. Restarting automation controller service"
 
-        - name: restart the controller service
-          ansible.builtin.service:
-            name: automation-controller
-            state: restarted
+#         - name: restart the controller service
+#           ansible.builtin.service:
+#             name: automation-controller
+#             state: restarted
 
-        - name: Ensure tower/controller is online and working
-          uri:
-            url: https://localhost/api/v2/ping/
-            method: GET
-            user: "{{ admin_username }}"
-            password: "{{ admin_password }}"
-            validate_certs: false
-            force_basic_auth: true
-          register: controller_online
-          until: controller_online is success
-          delay: 3
-          retries: 5
+#         - name: Ensure tower/controller is online and working
+#           uri:
+#             url: https://localhost/api/v2/ping/
+#             method: GET
+#             user: "{{ admin_username }}"
+#             password: "{{ admin_password }}"
+#             validate_certs: false
+#             force_basic_auth: true
+#           register: controller_online
+#           until: controller_online is success
+#           delay: 3
+#           retries: 5
 
-        - name: Retry getting auth token
-          ansible.controller.token:
-            description: 'Instruqt lab'
-            scope: "write"
-            state: present
-            controller_host: controller
-            controller_username: "{{ controller_admin_user }}"
-            controller_password: "{{ controller_admin_password }}"
-            validate_certs: false
-          register: _auth_token
-          until: _auth_token is not failed
-          delay: 3
-          retries: 5
-      always:
-        - name: Create fact.d dir
-          ansible.builtin.file:
-            path: "{{ custom_facts_dir }}"
-            state: directory
-            recurse: yes
-            owner: "{{ ansible_user }}"
-            group: "{{ ansible_user }}"
-            mode: 0755
-          become: true
+#         - name: Retry getting auth token
+#           ansible.controller.token:
+#             description: 'Instruqt lab'
+#             scope: "write"
+#             state: present
+#             controller_host: controller
+#             controller_username: "{{ controller_admin_user }}"
+#             controller_password: "{{ controller_admin_password }}"
+#             validate_certs: false
+#           register: _auth_token
+#           until: _auth_token is not failed
+#           delay: 3
+#           retries: 5
+#       always:
+#         - name: Create fact.d dir
+#           ansible.builtin.file:
+#             path: "{{ custom_facts_dir }}"
+#             state: directory
+#             recurse: yes
+#             owner: "{{ ansible_user }}"
+#             group: "{{ ansible_user }}"
+#             mode: 0755
+#           become: true
 
-        - name: Create _auth_token custom fact
-          ansible.builtin.copy:
-            content: "{{ _auth_token.ansible_facts }}"
-            dest: "{{ custom_facts_dir }}/{{ custom_facts_file }}"
-            owner: "{{ ansible_user }}"
-            group: "{{ ansible_user }}"
-            mode: 0644
-          become: true
-      check_mode: false
-      when: ansible_local.custom_facts.controller_token is undefined
-      tags:
-        - auth-token
+#         - name: Create _auth_token custom fact
+#           ansible.builtin.copy:
+#             content: "{{ _auth_token.ansible_facts }}"
+#             dest: "{{ custom_facts_dir }}/{{ custom_facts_file }}"
+#             owner: "{{ ansible_user }}"
+#             group: "{{ ansible_user }}"
+#             mode: 0644
+#           become: true
+#       check_mode: false
+#       when: ansible_local.custom_facts.controller_token is undefined
+#       tags:
+#         - auth-token
 
-    - name: refresh facts
-      setup:
-        filter:
-          - ansible_local
-      tags:
-        - always
+#     - name: refresh facts
+#       setup:
+#         filter:
+#           - ansible_local
+#       tags:
+#         - always
 
-    - name: create auth token fact
-      ansible.builtin.set_fact:
-        auth_token: "{{ ansible_local.custom_facts.controller_token }}"
-        cacheable: true
-      check_mode: false
-      when: auth_token is undefined
-      tags:
-        - always
+#     - name: create auth token fact
+#       ansible.builtin.set_fact:
+#         auth_token: "{{ ansible_local.custom_facts.controller_token }}"
+#         cacheable: true
+#       check_mode: false
+#       when: auth_token is undefined
+#       tags:
+#         - always
  
-    - name: Gather service facts
-      service_facts:
+#     - name: Gather service facts
+#       service_facts:
 
-    - name: Ensure tower/controller is online and working
-      uri:
-        url: https://{{ controller_hostname }}/api/v2/ping/
-        method: GET
-        user: "{{ admin_username }}"
-        password: "{{ admin_password }}"
-        validate_certs: false
-        force_basic_auth: true
-      register: controller_online
-      until: controller_online is success
-      delay: 3
-      retries: 5
-      when: "'automation-controller' in ansible_facts.services"
-      tags:
-        - controller-config
+#     - name: Ensure tower/controller is online and working
+#       uri:
+#         url: https://{{ controller_hostname }}/api/v2/ping/
+#         method: GET
+#         user: "{{ admin_username }}"
+#         password: "{{ admin_password }}"
+#         validate_certs: false
+#         force_basic_auth: true
+#       register: controller_online
+#       until: controller_online is success
+#       delay: 3
+#       retries: 5
+#       when: "'automation-controller' in ansible_facts.services"
+#       tags:
+#         - controller-config
 
-# Controller objects
-    - name: Check controller user
-      uri:
-        url: https://{{ controller_hostname }}/api/v2/me/
-        method: GET
-        user: "{{ controller_admin_user }}"
-        password: "{{ controller_admin_password }}"
-        validate_certs: false
-        force_basic_auth: true
-      register: me
-      when: "'automation-controller' in ansible_facts.services"
-      tags:
-        - controller-config
+# # Controller objects
+#     - name: Check controller user
+#       uri:
+#         url: https://{{ controller_hostname }}/api/v2/me/
+#         method: GET
+#         user: "{{ controller_admin_user }}"
+#         password: "{{ controller_admin_password }}"
+#         validate_certs: false
+#         force_basic_auth: true
+#       register: me
+#       when: "'automation-controller' in ansible_facts.services"
+#       tags:
+#         - controller-config
 
-    - name: Fail if not superuser
-      ansible.builtin.fail:
-        msg: "Controller user lacks superuser; cannot create org."
-      when: not (me.json.is_superuser | default(false))
-      tags:
-        - controller-config
+#     - name: Debug controller user
+#       ansible.builtin.debug:
+#         var: me
 
-    - name: Add Organization
-      ansible.controller.organization:
-        name: "{{ lab_organization }}"
-        description: "ACME Corp Organization"
-        state: present
-        controller_host: "https://{{ controller_hostname }}"
-        controller_username: "{{ controller_admin_user }}"
-        controller_password: "{{ controller_admin_password }}"
-        validate_certs: false
-      tags:
-        - controller-config
-        - controller-org
+    # - name: Fail if not superuser
+    #   ansible.builtin.fail:
+    #     msg: "Controller user lacks superuser; cannot create org."
+    #   when: not (me.json.is_superuser | default(false))
+    #   tags:
+    #     - controller-config
+
+    # - name: Add Organization
+    #   ansible.controller.organization:
+    #     name: "{{ lab_organization }}"
+    #     description: "ACME Corp Organization"
+    #     state: present
+    #     controller_host: "https://{{ controller_hostname }}"
+    #     controller_username: "{{ controller_admin_user }}"
+    #     controller_password: "{{ controller_admin_password }}"
+    #     validate_certs: false
+    #   tags:
+    #     - controller-config
+    #     - controller-org
   
     - name: Add Instruqt Windows EE
       ansible.controller.execution_environment:
@@ -481,19 +485,19 @@ cat <<EOF | tee /tmp/controller-setup.yml
         - controller-config
         - controller-ees
 
-    - name: Create student admin user
-      ansible.controller.user:
-        superuser: true
-        username: "{{ student_user }}"
-        password: "{{ student_password }}"
-        email: student@acme.example.com
-        controller_host: "https://{{ controller_hostname }}"
-        controller_username: "{{ controller_admin_user }}"
-        controller_password: "{{ controller_admin_password }}"
-        validate_certs: "{{ controller_validate_certs }}"
-      tags:
-        - controller-config
-        - controller-users    
+    # - name: Create student admin user
+    #   ansible.controller.user:
+    #     superuser: true
+    #     username: "{{ student_user }}"
+    #     password: "{{ student_password }}"
+    #     email: student@acme.example.com
+    #     controller_host: "https://{{ controller_hostname }}"
+    #     controller_username: "{{ controller_admin_user }}"
+    #     controller_password: "{{ controller_admin_password }}"
+    #     validate_certs: "{{ controller_validate_certs }}"
+    #   tags:
+    #     - controller-config
+    #     - controller-users    
         
     - name: Create Inventory
       ansible.controller.inventory:
@@ -1158,4 +1162,4 @@ EOF
 # EOF
 
 ## TEST 
-ANSIBLE_COLLECTIONS_PATH=/root/.ansible/collections/ansible_collections/ ansible-playbook -e @/tmp/track-vars.yml -i /tmp/inventory /tmp/controller-setup.yml
+ANSIBLE_COLLECTIONS_PATH=/root/.ansible/collections/ansible_collections/ ansible-playbook -e @/tmp/track-vars.yml -i /tmp/inventory /tmp/controller-setup.yml -vv
