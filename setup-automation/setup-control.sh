8eb8de47d9d652687e3e032eeaf036fbf27d676a
amoyament <129310912+amoyament@users.noreply.github.com>
2025-10-28 16:37:51 -0400

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -183,11 +183,12 @@ cat <<'EOF' | tee /tmp/windows-setup.yml
       args:
         executable: powershell.exe
 
-    - name: Execute slmgr /rearm as SYSTEM
+- name: Execute slmgr /rearm as SYSTEM
       ansible.windows.win_command: >
         cscript.exe //B //NoLogo %windir%\system32\slmgr.vbs /rearm
       become: yes
-      become_method: machine  
+      become_method: powershell
+      become_user: System
       register: rearm_result
 
     - name: Reboot after Chocolatey/slmgr setup
